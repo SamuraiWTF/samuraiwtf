ed2f0022aaf4144fbe455beec87128dcec1086bb
Cory Sabol <cory@secureideas.com>
2018-04-16 19:48:21 -0400

    pull and launch dockerized dvwa image

--- config/startup_targets.sh
+++ config/startup_targets.sh
@@ -2,6 +2,8 @@
 
 sudo docker run --rm -p 3000:3000 bkimminich/juice-shop &
 
+sudo docker run --rm -p 31000:80 -p 33006:3306 bit0pus/docker-dwva &
+
 cd /opt/targets/samurai-dojo-docker
 sudo docker-compose up &
 
