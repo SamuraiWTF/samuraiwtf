5115834086798e9e48d32bb7db82788ac2722dc9
Cory Sabol <cory@secureideas.com>
2018-04-17 19:48:42 -0400

    startup the mutillidae docker image

--- config/startup_targets.sh
+++ config/startup_targets.sh
@@ -4,6 +4,8 @@ sudo docker run --rm -p 3000:3000 bkimminich/juice-shop &
 
 sudo docker run --rm -p 31000:80 -p 33006:3306 bit0pus/docker-dvwa &
 
+sudo docker run --rm -p 33080:80 -p 22222:22 -p 33333:3306 bit0pus/docker-mutillidae &
+
 cd /opt/targets/samurai-dojo-docker
 sudo docker-compose up &
 
