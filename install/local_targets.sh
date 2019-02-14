2937e721895499e96fea88f93eb3e98fe1925212
Mic <mic@secureideas.com>
2019-02-14 13:54:03 -0500

    Added cors demonstrator.

--- install/local_targets.sh
+++ install/local_targets.sh
@@ -9,6 +9,8 @@ echo '127.0.0.1   dvwa.wtf' | sudo tee -a /etc/hosts
 echo '127.0.0.1   mutillidae.wtf' | sudo tee -a /etc/hosts
 echo '127.0.0.1   professionallyevil.wtf' | sudo tee -a /etc/hosts
 echo '127.0.0.1   amoksecurity.wtf' | sudo tee -a /etc/hosts
+echo '127.0.0.1   client.cors.dem' | sudo tee -a /etc/hosts
+echo '127.0.0.1   api.cors.dem' | sudo tee -a /etc/hosts
 
 sudo mkdir /home/samurai/.scripts
 sudo tr '\r\n' '\n' < /tmp/config/startup_targets.sh > /home/samurai/.scripts/startup_targets.sh
