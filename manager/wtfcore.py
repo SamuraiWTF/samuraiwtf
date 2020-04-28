c3b80bb75ddb88e1cdb0c70cb0b520e2d8bf7361
Jason Gillam <jgillam@secureideas.com>
2020-04-28 11:54:28 -0400

    Reformat for easier reading.

--- manager/wtfcore.py
+++ manager/wtfcore.py
@@ -1,72 +1,73 @@
 from os import listdir
 from os.path import isdir, join, dirname, realpath, abspath
-import subprocess
 import yaml
 import wtferrors
 
 module_dict = {}
 
+
 def load_module_info(path):
-  with open(path, 'r') as stream:
-    module_info = yaml.load(stream, Loader=yaml.SafeLoader)
-    module_info['path'] = dirname(path)
+    with open(path, 'r') as stream:
+        module_info = yaml.load(stream, Loader=yaml.SafeLoader)
+        module_info['path'] = dirname(path)
 
-    provisioner_class = module_info.get("class", "provisioners.DefaultProvisioner")
-    if "." in provisioner_class:
-      classname = provisioner_class[provisioner_class.rindex(".")+1:]
-    else:
-      classname = provisioner_class
+        provisioner_class = module_info.get("class", "provisioners.DefaultProvisioner")
+        if "." in provisioner_class:
+            class_name = provisioner_class[provisioner_class.rindex(".") + 1:]
+        else:
+            class_name = provisioner_class
 
-    mod = __import__(provisioner_class, fromlist=[classname])
-    klass = getattr(mod, classname)
-    
-    provisioner = klass(module_info)
-    module_dict[module_info.get('name').lower()] = provisioner
+        mod = __import__(provisioner_class, fromlist=[class_name])
+        klass = getattr(mod, class_name)
 
-    return provisioner
+        provisioner = klass(module_info)
+        module_dict[module_info.get('name').lower()] = provisioner
+
+        return provisioner
+
 
 def list_modules(path=None, module_list=[]):
-  if path is None:
-    my_path = abspath(dirname(__file__))
-    path = realpath(join(my_path, "../modules"))
+    if path is None:
+        my_path = abspath(dirname(__file__))
+        path = realpath(join(my_path, "../modules"))
 
+    if len(module_list) == 0:
+        module_dict.clear()
 
-  if len(module_list) == 0:
-    module_dict.clear()
+    file_list = listdir(path)
+    for f in file_list:
+        file_path = join(path, f)
+        if isdir(file_path):
+            list_modules(file_path, module_list)
+        elif f.endswith(".yml"):
+            module_info = load_module_info(file_path)
+            if module_info is not None:
+                module_list.append(module_info)
+    return module_list
 
-  file_list = listdir(path)
-  for f in file_list:
-    file_path = join(path, f)
-    if isdir(file_path):
-      list_modules(file_path, module_list)
-    elif f.endswith(".yml"):
-      module_info = load_module_info(file_path)
-      if module_info is not None:
-        module_list.append(module_info)
-  return module_list
 
 def get_module_info(name):
-  return module_dict.get(name)
+    return module_dict.get(name)
+
 
 def _run_function(module_name, function_name):
-  if len(module_dict) == 0:
-    list_modules()
+    if len(module_dict) == 0:
+        list_modules()
 
-  provisioner = module_dict.get(module_name.lower())
-  if provisioner is None:
-    raise wtferrors.ModuleNotFound(name)
+    provisioner = module_dict.get(module_name.lower())
+    if provisioner is None:
+        raise wtferrors.ModuleNotFound(name)
 
-  if hasattr(provisioner, function_name) and callable(getattr(provisioner, function_name)):
-    function_to_call = getattr(provisioner, function_name)
-    function_to_call()    
-  else:
-    raise wtferrors.NotImplemented(function_name, type(provisioner).__name__)
-
+    if hasattr(provisioner, function_name) and callable(getattr(provisioner, function_name)):
+        function_to_call = getattr(provisioner, function_name)
+        function_to_call()
+    else:
+        raise wtferrors.NotImplemented(function_name, type(provisioner).__name__)
 
 
 def install_module(name):
-  _run_function(name, "install")
+    _run_function(name, "install")
 
 
 def remove_module(name):
-  _run_function(name, "remove")
+    _run_function(name, "remove")
--- manager/wtferrors.py
+++ manager/wtferrors.py
@@ -1,26 +1,30 @@
 class WTFError(Exception):
-  pass
+    pass
+
 
 class ModuleNotFound(WTFError):
 
-  def __init__(self, module):
-    self.message = "Module not found: {}".format(module)
+    def __init__(self, module):
+        self.message = "Module not found: {}".format(module)
+
 
 class NotImplemented(WTFError):
 
-  def __init__(self, name, provisioner):
-    self.message = "Function '{}' is not implemented for the provisioner {}.".format(name, provisioner)
+    def __init__(self, name, provisioner):
+        self.message = "Function '{}' is not implemented for the provisioner {}.".format(name, provisioner)
 
-  def __init__(self, name, provisioner, module):
-    self.message = "Function '{}' is not implemented in the module '{}'.".format(name, module)
+    def __init__(self, name, provisioner, module):
+        self.message = "Function '{}' is not implemented in the module '{}'.".format(name, module)
+
 
 class MissingFunction(WTFError):
 
-  def __init__(self, func, task_type):
-    self.message = "Function '{}' is missing from the '{}' plugin.".format(func, task_type)
+    def __init__(self, func, task_type):
+        self.message = "Function '{}' is missing from the '{}' plugin.".format(func, task_type)
 
 
 class MissingRequiredParam(WTFError):
 
-  def __init__(self, param, plugin_name):
-    self.message = "Plugin '{}' requires parameter '{}', but this parameter appears to be missing.".format(plugin_name, param)
+    def __init__(self, param, plugin_name):
+        self.message = "Plugin '{}' requires parameter '{}', but this parameter appears to be missing.".format(
+            plugin_name, param)
