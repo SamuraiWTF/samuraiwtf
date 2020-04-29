0c80c328674b4706d3858c39578c27baed41d31b
Jason Gillam <jgillam@secureideas.com>
2020-04-29 16:51:01 -0400

    Burpsuite community edition installs

---
+++ manager/plugins/File.py
@@ -0,0 +1,28 @@
+from plugins import Plugin
+import os
+import wtferrors
+
+
+class File(Plugin):
+
+    @classmethod
+    def get_aliases(cls):
+        return ["file"]
+
+    def any(self, params):
+        self._validate_params(params, ['path', 'state'], 'file')
+
+        state = params.get('state')
+        if state not in ['directory']:
+            raise wtferrors.UnrecognizedParamValue('state', state, 'file', 'directory')
+
+        if state == 'directory':
+            try:
+                if params.get('mode') is None:
+                    os.makedirs(params.get('path'))
+                    return True, None
+                else:
+                    os.makedirs(params.get('path'), mode=params.get('mode'))
+                    return True, None
+            except FileExistsError as err:
+                return False, 'Specified path exists.'
---
+++ manager/plugins/GetUrl.py
@@ -0,0 +1,26 @@
+from plugins import Plugin
+import requests
+import os
+import os.path
+import wtferrors
+
+
+class GetUrl(Plugin):
+
+    @classmethod
+    def get_aliases(cls):
+        return ["get_url"]
+
+    def any(self, params):
+        self._validate_params(params, ['url', 'dest'], 'get_url')
+
+        if os.path.exists(params.get('dest')) and not params.get('overwrite', False):
+            return False, 'The specified file already exists: {}'.format(params.get('dest'))
+        else:
+            r = requests.get(params.get('url'), stream=True)
+
+            with open(params.get("dest"), "wb") as output:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        output.write(chunk)
+            return True, None
