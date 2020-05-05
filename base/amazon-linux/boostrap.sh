560bd5b1b2ffe675ebc3128fcfd3eb026205de3d
Jason Gillam <jgillam@secureideas.com>
2020-05-05 18:51:24 -0400

    Move katana to /opt/katana and provide a --update function on the launcher to pull the latest from 5.0 branch.

--- base/amazon-linux/boostrap.sh
+++ base/amazon-linux/boostrap.sh
@@ -2,14 +2,15 @@
 
 sudo amazon-linux-extras install ansible2
 
-pushd ~
+pushd ~ || exit
 
-git clone --depth=1 --single-branch --branch amazon-linux https://github.com/SamuraiWTF/samuraiwtf.git
+git clone --depth=1 --single-branch --branch --branch "5.0-dev" https://github.com/SamuraiWTF/samuraiwtf.git
 
-cd samuraiwtf
+cd samuraiwtf || exit
 
 sudo ansible-playbook -K base/amazon-linux/local_playbook.yml
 
-sudo ln -s $(pwd)/katana/ /opt/samurai/katana
+sudo mkdir -p /opt/katana
+sudo cp -R katana/* /opt/katana/
 
 popd
