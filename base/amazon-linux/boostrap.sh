de6ac5d21ad9297ae47a13f2e72896cb0ee7e51c
Jason Gillam <jgillam@secureideas.com>
2020-05-06 10:20:56 -0400

    fixed error in bootstrap, removed git clone since this should already be done before we bootstrap.

--- base/amazon-linux/boostrap.sh
+++ base/amazon-linux/boostrap.sh
@@ -2,10 +2,6 @@
 
 sudo amazon-linux-extras install ansible2
 
-pushd ~ || exit
-
-git clone --depth=1 --single-branch --branch --branch "5.0-dev" https://github.com/SamuraiWTF/samuraiwtf.git
-
 cd samuraiwtf || exit
 
 sudo ansible-playbook -K base/amazon-linux/local_playbook.yml
@@ -16,5 +12,3 @@ else
   sudo mkdir -p /opt/katana
   sudo cp -R katana/* /opt/katana/
 fi
-
-popd
