19364425e7eda8847d9898ac34eb0c1026683ed5
Jason Gillam <jgillam@secureideas.com>
2020-05-06 09:10:17 -0400

    Add --develop switch to link katana instead of copying it.

--- base/amazon-linux/boostrap.sh
+++ base/amazon-linux/boostrap.sh
@@ -10,7 +10,11 @@ cd samuraiwtf || exit
 
 sudo ansible-playbook -K base/amazon-linux/local_playbook.yml
 
-sudo mkdir -p /opt/katana
-sudo cp -R katana/* /opt/katana/
+if [ $1 = "--develop" ]; then
+  sudo ln -s "$(pwd)/katana/" /opt/katana
+else
+  sudo mkdir -p /opt/katana
+  sudo cp -R katana/* /opt/katana/
+fi
 
 popd
