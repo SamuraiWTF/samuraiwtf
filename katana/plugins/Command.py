b8fb841fb22dfcc5eb259ad0f2283bf800522a95
Cory Sabol <cory@secureideas.com>
2020-05-08 11:03:19 -0400

    added docstring to command.py; added 2 new options: unsafe and shell

--- katana/plugins/Command.py
+++ katana/plugins/Command.py
@@ -7,7 +7,22 @@ import os
 
 
 class Command(Plugin):
-
+    '''
+    module: command
+    options:
+        cmd:
+            type: str
+            description:
+                - the command to run
+        shell:
+            type: bool
+            description:
+                - When true the command is executed with a shell, allowing for shell expansions to work.
+        unsafe:
+            type: bool
+            description:
+                - When true the command string is passed straight into the subprocess call.
+    '''
     @classmethod
     def get_aliases(cls):
         return ["command"]
@@ -15,8 +30,11 @@ class Command(Plugin):
     def any(self, params):
         self._validate_params(params, ['cmd'], 'command')
 
-        command_list = shlex.split(params.get('cmd'))
-        results = subprocess.run(command_list, cwd=params.get('cwd'))
+        command_list = params.get('cmd')
+        shell = params.get('shell')
+        if not params.get('unsafe'):
+            command_list = shlex.split(command_list)
+        results = subprocess.run(command_list, shell=shell, cwd=params.get('cwd')) 
 
         return True, None
 
