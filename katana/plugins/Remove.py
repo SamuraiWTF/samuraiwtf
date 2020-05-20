4cf350d882a4e556a260374510e545e0bbf00128
Jason Gillam <jgillam@secureideas.com>
2020-05-19 22:01:31 -0400

    Added support for lists of paths

--- katana/plugins/Remove.py
+++ katana/plugins/Remove.py
@@ -10,13 +10,27 @@ class Remove(Plugin):
     def get_aliases(cls):
         return ["rm"]
 
+    @staticmethod
+    def _remove(path):
+        if os.path.exists(path):
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
+            return True
+        else:
+            return False
+
     def any(self, params):
         self._validate_params(params, ['path'], 'rm')
-        if os.path.exists(params.get("path")):
-            if os.path.isfile(params.get("path")):
-                os.remove(params.get("path"))
-            elif os.path.isdir(params.get("path")):
-                shutil.rmtree(params.get("path"))
+        if isinstance(params.get("path"), str):
+            result = self._remove(params.get("path"))
+        else:
+            result = False
+            for path in params.get("path"):
+                result = result or self._remove(path)
+
+        if result:
             return True, None
         else:
             return False, "Nothing to remove. Path does not exist: {}".format(params.get("path"))
