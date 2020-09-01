168f551b63a6ae2c94697ef38e6c52008ae0fd5c
Jason Gillam <jasongillam@jasons-mbp.lan>
2020-09-01 11:31:47 -0400

    Moved katana to its own repo

--- katana/html/css/readme.txt
+++
@@ -1,3 +0,0 @@
-bulma.min.css is from Bulma version 0.8.2 (see Bulma_LICENCE.txt)
-all.css is from fontawesome free 5.13.0 (see fa_LICENSE.txt)
-The ../webfonts folder is also from fontawesome.
--- katana/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-from .Plugin import Plugin
-from .Copy import Copy
-from .Docker import Docker
-from .Git import Git
-from .Remove import Remove
-from .Command import Command
-from .Unarchive import Unarchive
