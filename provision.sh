e3d03da4afc48a1420e06b45bb05d245e3e6686d
Jason Gillam <jasongillam@jasons-mbp.lan>
2020-09-16 12:34:24 -0400

    Most finishing touches are now in the provision script

--- provision.sh
+++ provision.sh
@@ -2,12 +2,50 @@
 
 echo "Vagrant Provisioning for Ubuntu"
 
-apt install -y python-is-python3
+apt install -y python-is-python3 gnome-shell-extension-arc-menu gnome-tweaks
 
 chown samurai:samurai /opt/katana
 
+cp /vagrant/base/common/*.png /opt/samurai/
+
+echo "Setting up first-time login script."
+rm -f /etc/profile.d/first_login.sh
+cat <<EOT >> /etc/profile.d/first_login.sh
+#!/bin/bash
+
+if [ -e ~/.samurai ]
+then
+  echo "Skipping first run: already run first time scripts."
+else
+  cd /etc/dconf
+  /usr/bin/dconf write /org/mate/desktop/background/picture-filename "'/opt/samurai/samurai-background.png'"
+  /usr/bin/dconf write /org/mate/desktop/background/picture-options "'stretched'"
+  /usr/bin/dconf write /org/gnome/desktop/background/picture-uri "'file:///opt/samurai/samurai-background.png'"
+  /usr/bin/dconf write /org/mate/terminal/profiles/default/custom-command "'bash'"
+  /usr/bin/dconf write /org/mate/terminal/profiles/default/use-custom-command true
+  /usr/bin/dconf write /org/gnome/shell/enabled-extensions "['arc-menu@linxgem33.com']"
+  /usr/bin/dconf write /org/gnome/shell/extensions/arc-menu/dtp-dtd-state "[false, false]"
+  /usr/bin/dconf write /org/gnome/mutter/overlay-key "'Super_L'"
+  /usr/bin/dconf write /org/gnome/desktop/wm/keybindings/panel-main-menu "['<Alt>F1']"
+  /usr/bin/dconf write /org/gnome/shell/extensions/arc-menu/menu-button-appearance "'Text'"
+  /usr/bin/dconf write /org/gnome/shell/extensions/arc-menu/enable-pinned-apps false
+
+  if [ ! -L ~/samurai ]; then
+    ln -s /opt/samurai ~/samurai
+  fi
+  touch ~/.samurai
+
+fi
+EOT
+
+## TODO: this doesn't seem to work
+cat <<EOT >> /etc/xdg/autostart/samuraisize.desktop
+sleep 20 && xrand --size 1440x900
+EOT
+
+
 echo "Installing Katana launcher..."
-rm -rf /usr/bin/katana
+rm -f /usr/bin/katana
 cat <<EOT >> /usr/bin/katana
 #!/usr/bin/bash
 if [[ "\$1" = "--update" ]]; then
@@ -18,10 +56,9 @@ if [[ "\$1" = "--update" ]]; then
   sudo git clone --depth=1 --single-branch --branch "master" https://github.com/SamuraiWTF/katana.git || exit
   sudo mkdir -p /opt/katana
   sudo cp -R /tmp/katana/* /opt/katana/
-  sudo chown -R root:root /opt/katana
-  pushd /opt/katana
-  pip install -r requirements.txt
-  popd
+  sudo chown -R samurai:root /opt/katana
+  cd /opt/katana
+  sudo pip install -r requirements.txt
   popd
   echo "Update is complete."
 else
