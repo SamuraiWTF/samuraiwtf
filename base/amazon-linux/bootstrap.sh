d85b0413ec3fc0122d5ed2fbd929d1bc95b6566d
jgillam <jason@secureideas.com>
2022-02-16 14:08:51 -0500

    Added rootCA and initial katana cert generation to address #36.

--- base/amazon-linux/bootstrap.sh
+++ base/amazon-linux/bootstrap.sh
@@ -8,7 +8,14 @@ pushd "$DIR"/../.. || exit
 
 sudo ansible-playbook -K base/amazon-linux/local_playbook.yml
 
-katana --update
+sudo openssl genrsa -out /etc/samurai.d/certs/rootCAKey.pem 2048
+sudo openssl req -x509 -sha256 -new -nodes -key /etc/samurai.d/certs/rootCAKey.pem -days 365 -out /etc/samurai.d/certs/rootCACert.pem -subj "/C=US/ST=Hacking/L=Springfield/O=SamuraiWTF/CN=samuraiwtf"
+sudo cp /etc/samurai.d/certs/rootCACert.pem /etc/pki/ca-trust/source/anchors/
+sudo update-ca-trust
+sudo openssl req -new -newkey rsa:4096 -nodes -keyout /etc/samurai.d/certs/katana.test.key -out /etc/samurai.d/certs/katana.test.csr -subj "/C=US/ST=Hacking/L=Springfield/O=SamuraiWTF/CN=katana.test"
+
+
+katana --update updates/rollup2022-02-rollup
 
 katana install katana
 
